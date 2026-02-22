4ef63164f0fe441518ffa0e6867e962bc29e5f12
meyyy <aisyahumairah@users.noreply.github.com>
2026-02-22 10:01:46 +0800

    feat: add show/hide password + checking pswd strength at create new user

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -53,18 +53,46 @@
 
                             <div class="form-group row mb-3">
                                 <label class="col-form-label col-md-3 col-sm-3 label-align">Initial Password</label>
-                                <div class="col-md-6 col-sm-6">
-                                    <input type="password" name="password" class="form-control">
-                                    <small class="text-muted">Leave blank to use the system default password:
+                                <div class="col-md-6 col-sm-6 position-relative">
+                                    <input type="password" id="password" name="password" class="form-control">
+                                    <span class="toggle-password position-absolute"
+                                        style="right: 25px; top: 10px; cursor: pointer;">
+                                        <i class="fa fa-eye"></i>
+                                    </span>
+                                    <small class="text-muted d-block mt-1">Leave blank to use the system default password:
                                         <strong>abc123</strong>. The user will be required to change it on first
                                         login.</small>
+
+                                    <div class="mt-2" id="password-strength" style="display: none;">
+                                        <div class="progress" style="height: 10px;">
+                                            <div id="strength-bar" class="progress-bar" role="progressbar"
+                                                style="width: 0%;"></div>
+                                        </div>
+                                        <ul class="list-unstyled mt-2 small">
+                                            <li id="length" class="text-danger"><i class="fa fa-times me-1"></i> 8-12
+                                                characters</li>
+                                            <li id="upper" class="text-danger"><i class="fa fa-times me-1"></i> At least
+                                                one uppercase letter (A-Z)</li>
+                                            <li id="lower" class="text-danger"><i class="fa fa-times me-1"></i> At least
+                                                one lowercase letter (a-z)</li>
+                                            <li id="number" class="text-danger"><i class="fa fa-times me-1"></i> At least
+                                                one number (0-9)</li>
+                                            <li id="symbol" class="text-danger"><i class="fa fa-times me-1"></i> At least
+                                                one special character (@$!%*#?&.)</li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
 
                             <div class="form-group row mb-3">
                                 <label class="col-form-label col-md-3 col-sm-3 label-align">Confirm Password</label>
-                                <div class="col-md-6 col-sm-6">
-                                    <input type="password" name="password_confirmation" class="form-control">
+                                <div class="col-md-6 col-sm-6 position-relative">
+                                    <input type="password" id="password_confirmation" name="password_confirmation"
+                                        class="form-control">
+                                    <span class="toggle-password position-absolute"
+                                        style="right: 25px; top: 10px; cursor: pointer;">
+                                        <i class="fa fa-eye"></i>
+                                    </span>
                                 </div>
                             </div>
 
@@ -72,7 +100,7 @@
                             <div class="form-group row">
                                 <div class="col-md-9 col-sm-9 offset-md-3">
                                     <a href="{{ route('admin.users.index') }}" class="btn btn-secondary">Cancel</a>
-                                    <button type="submit" class="btn btn-success">Create User</button>
+                                    <button type="submit" id="submit-btn" class="btn btn-success">Create User</button>
                                 </div>
                             </div>
                         </form>
@@ -81,4 +109,82 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready(function() {
+            // Toggle Password Visibility
+            $('.toggle-password').on('click', function() {
+                const input = $(this).siblings('input');
+                const icon = $(this).find('i');
+
+                if (input.attr('type') === 'password') {
+                    input.attr('type', 'text');
+                    icon.removeClass('fa-eye').addClass('fa-eye-slash');
+                } else {
+                    input.attr('type', 'password');
+                    icon.removeClass('fa-eye-slash').addClass('fa-eye');
+                }
+            });
+
+            // Password Strength Checker
+            $('#password').on('input', function() {
+                const password = $(this).val();
+
+                if (password.length > 0) {
+                    $('#password-strength').show();
+                } else {
+                    $('#password-strength').hide();
+                    $('#submit-btn').prop('disabled', false); // Enable if blank (optional)
+                    return;
+                }
+
+                let strength = 0;
+
+                // Validation Criteria
+                const criteria = {
+                    length: password.length >= 8 && password.length <= 12,
+                    upper: /[A-Z]/.test(password),
+                    lower: /[a-z]/.test(password),
+                    number: /[0-9]/.test(password),
+                    symbol: /[@$!%*#?&.]/.test(password)
+                };
+
+                // Update UI for each criterion
+                Object.keys(criteria).forEach(key => {
+                    const element = $('#' + key);
+                    if (criteria[key]) {
+                        element.removeClass('text-danger').addClass('text-success');
+                        element.find('i').removeClass('fa-times').addClass('fa-check');
+                        strength++;
+                    } else {
+                        element.removeClass('text-success').addClass('text-danger');
+                        element.find('i').removeClass('fa-check').addClass('fa-times');
+                    }
+                });
+
+                // Update Strength Bar
+                const bar = $('#strength-bar');
+                const percentage = (strength / 5) * 100;
+                bar.css('width', percentage + '%');
+
+                if (strength <= 2) {
+                    bar.removeClass('bg-success bg-warning').addClass('bg-danger');
+                } else if (strength <= 4) {
+                    bar.removeClass('bg-danger bg-success').addClass('bg-warning');
+                } else {
+                    bar.removeClass('bg-danger bg-warning').addClass('bg-success');
+                }
+
+                // Enable/Disable submit button
+                // Only disable if there is input but it's not strong enough
+                if (strength === 5) {
+                    $('#submit-btn').prop('disabled', false);
+                } else {
+                    $('#submit-btn').prop('disabled', true);
+                }
+            });
+        });
+    </script>
 @endsection
